f0484335d07767272343a3b18db3108de6604307
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-08-09 05:53:08 +0300

    Initial Commit

---
+++ Africa-SwiftUI/Africa_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_SwiftUIApp.swift
+//  Africa-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Africa_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Africa-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
